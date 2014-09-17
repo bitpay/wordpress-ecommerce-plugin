d0b4ccbc3d99db3f9f2e447d7a5ce89985c361f4
Alexander Leitner <bitpay@bitpay.bp>
2014-09-17 17:49:10 -0400

    Fixed Database bug

--- wp-content/plugins/wp-e-commerce/wpsc-merchants/bitpay.merchant.php
+++ wp-content/plugins/wp-e-commerce/wpsc-merchants/bitpay.merchant.php
@@ -180,7 +180,7 @@ function gateway_bitpay($seperator, $sessionid)
 		`" . WPSC_TABLE_SUBMITED_FORM_DATA . "` ON
 		`" . WPSC_TABLE_CHECKOUT_FORMS . "`.id =
 		`" . WPSC_TABLE_SUBMITED_FORM_DATA . "`.`form_id` WHERE
-		`" . WPSC_TABLE_SUBMITED_FORM_DATA . "`.`log_id`=" . $purchase_log['id'];
+		`" . WPSC_TABLE_SUBMITED_FORM_DATA . "`.`log_id`='" . $purchase_log['id'] . "'";
 
 	$userinfo = $wpdb->get_results($usersql, ARRAY_A);
 
