93544ff48b2b88060dc336ff7c1867631d6e046f
Rich Morgan <ionux@users.noreply.github.com>
2014-12-04 12:41:57 -0500

    Cleaned up formatting

--- tests/test-bitpay-merchant.php
+++ tests/test-bitpay-merchant.php
@@ -2,195 +2,205 @@
 
 include_once __DIR__ . '/../wordpress/wp-content/plugins/wp-e-commerce/wpsc-merchants/bitpay.merchant.php';
 
-class bitpaymerchantTest extends WP_UnitTestCase {
+class bitpaymerchantTest extends WP_UnitTestCase
+{
 
-  function testbitpay_js_init() {
+    public function testbitpay_js_init()
+    {
+        bitpay_js_init();
+    }
 
-    bitpay_js_init();
+    public function testdebuglog()
+    {
+        $contents = '';
 
-  }
+        debuglog($contents);
+    }
 
-  function testdebuglog() {
+    public function testcreate_table()
+    {
+        create_table();
 
-    $contents = '';
+        // Check if Table is there
+        // assert you don't change an existing table (functional test) Mock dbDelta
+    }
 
-    debuglog($contents);
+    public function testgenerate_keys()
+    {
+        generate_keys();
 
-  }
+        // Expect that methods were called
+    }
 
-  function testcreate_table() {
-    
-    create_table();
-    #Check if Table is there
-    # assert you don't change an existing table (functional test) Mock dbDelta
+    public function testcreate_client()
+    {
 
-  }
+        $network = "Testnet";
 
-	function testgenerate_keys() {
+        $private = $this->getMock('Bitpay\\PrivateKey');
 
-    generate_keys();
-    #Expect that methods were called
+        $private->expects($this->any())->method('__toString')
+                ->will($this->returnValue('3a1cb093db55fc9cc6f2e1efc3938e4e498d8b2557a975249a49e2aec70ad471'));
 
+        $public = $this->getMock('Bitpay\\PublicKey');
 
-  }
+        $public->expects($this->any())->method('__toString')
+                ->will($this->returnValue('03bb80b4391db1a7ba344fbe5421d87952a4b8934ca0865ae70591d1614e0f6fc8'));
 
-  function testcreate_client() {
+        $client = create_client($network, $public, $private);
 
-    $network = "Testnet";
-    $private = $this->getMock('Bitpay\\PrivateKey');
-    $private->expects($this->any())->method('__toString')
-            ->will($this->returnValue('3a1cb093db55fc9cc6f2e1efc3938e4e498d8b2557a975249a49e2aec70ad471'));
+        $expected_client = new \Bitpay\Client\Client();
 
-    $public = $this->getMock('Bitpay\\PublicKey');
-    $public->expects($this->any())->method('__toString')
-            ->will($this->returnValue('03bb80b4391db1a7ba344fbe5421d87952a4b8934ca0865ae70591d1614e0f6fc8'));
+        $expected_client->setNetwork(new Bitpay\Network\Testnet);
+        $expected_client->setPublicKey($public);
+        $expected_client->setPrivateKey($private);
+        $expected_client->setAdapter(new Bitpay\Client\Adapter\CurlAdapter());
 
-    $client = create_client($network, $public, $private);
+        $this->assertTrue($expected_client == $client);
+    }
 
-    $expected_client = new \Bitpay\Client\Client();
-    $expected_client->setNetwork(new Bitpay\Network\Testnet);
-    $expected_client->setPublicKey($public);
-    $expected_client->setPrivateKey($private);
-    $expected_client->setAdapter(new Bitpay\Client\Adapter\CurlAdapter());
+    public function testpairing()
+    {
 
-    $this->assertTrue($expected_client == $client);
+        $sin = $this->getMock('Bitpay\\SinKey');
 
-  }
+        $sin->expects($this->any())->method('__toString')
+                ->will($this->returnValue('TfLG7rjKVMa9AFBaBKrS6ti8XM62yJA4D4c'));
 
-  function testpairing() {
+        $client = $this->getMock('Bitpay\\Client\\Client');
+        $token  = $this->getMock('Bitpay\\TokenInterface');
 
-    $sin = $this->getMock('Bitpay\\SinKey');
-    $sin->expects($this->any())->method('__toString')
-            ->will($this->returnValue('TfLG7rjKVMa9AFBaBKrS6ti8XM62yJA4D4c'));
+        $client->expects($this->any())->method('createToken')->will($this->returnValue($token));
 
-    $client = $this->getMock('Bitpay\\Client\\Client');
-    $token = $this->getMock('Bitpay\\TokenInterface');
-    $client->expects($this->any())->method('createToken')->will($this->returnValue($token));
-    $pairing_code = 'pairing';
+        $pairing_code = 'pairing';
 
-    $expected_token = pairing($pairing_code, $client, $sin);
+        $expected_token = pairing($pairing_code, $client, $sin);
 
-    $this->assertTrue($token == $expected_token);
-  }
+        $this->assertTrue($token == $expected_token);
+    }
 
+    public function testsave_keys()
+    {
 
-  function testsave_keys() {
+        $network = "Testnet";
 
-    $network = "Testnet";
+        // mock the client library to return key array copy numbers from tool
+        $private = $this->getMock('Bitpay\\PrivateKey');
 
-    // mock the client library to return key array copy numbers from tool
-    $private = $this->getMock('Bitpay\\PrivateKey');
-    $private->expects($this->any())->method('__toString')
-            ->will($this->returnValue('3a1cb093db55fc9cc6f2e1efc3938e4e498d8b2557a975249a49e2aec70ad471'));
+        $private->expects($this->any())->method('__toString')
+                ->will($this->returnValue('3a1cb093db55fc9cc6f2e1efc3938e4e498d8b2557a975249a49e2aec70ad471'));
 
-    $public = $this->getMock('Bitpay\\PublicKey');
-    $public->expects($this->any())->method('__toString')
-            ->will($this->returnValue('03bb80b4391db1a7ba344fbe5421d87952a4b8934ca0865ae70591d1614e0f6fc8'));
+        $public = $this->getMock('Bitpay\\PublicKey');
 
-    $sin = $this->getMock('Bitpay\\SinKey');
-    $sin->expects($this->any())->method('__toString')
-            ->will($this->returnValue('TfLG7rjKVMa9AFBaBKrS6ti8XM62yJA4D4c'));
+        $public->expects($this->any())->method('__toString')
+                ->will($this->returnValue('03bb80b4391db1a7ba344fbe5421d87952a4b8934ca0865ae70591d1614e0f6fc8'));
 
-    $token = $this->getMock('Bitpay\\TokenInterface');
-    $token->expects($this->any())->method('getFacade')->will($this->returnValue('pos'));
-    
-    save_keys($token, $network, $private, $public, $sin);
+        $sin = $this->getMock('Bitpay\\SinKey');
 
-  }
+        $sin->expects($this->any())->method('__toString')
+                ->will($this->returnValue('TfLG7rjKVMa9AFBaBKrS6ti8XM62yJA4D4c'));
 
-  function testpair_and_get_token() {
+        $token = $this->getMock('Bitpay\\TokenInterface');
 
-    #Should not be tested
+        $token->expects($this->any())->method('getFacade')->will($this->returnValue('pos'));
 
-  }
+        save_keys($token, $network, $private, $public, $sin);
+    }
 
-  function testform_bitpay() {
+    public function testpair_and_get_token()
+    {
+        // Should not be tested
+    }
 
-    #Should be a functionallity test. Jasmine
+    public function testform_bitpay()
+    {
+        // Should be a functionallity test. Jasmine
+    }
 
-  }
+    public function testsubmit_bitpay()
+    {
+        // test cases for different button states
+        submit_bitpay();
+    }
 
-  function testsubmit_bitpay() {
+    public function testgateway_bitpay()
+    {
+        $separator = '?';
+        $sessionid = '0';
 
-    # test cases for different button states
-    submit_bitpay();
+        // gateway_bitpay($separator, $sessionid);
+    }
 
-  }
+    public function testbitpay_callback()
+    {
+        require_once "phpmockstream.php";
 
-  function testgateway_bitpay() {
+        $_GET['bitpay_callback'] = true;
 
-    $separator = '?';
-    $sessionid = '0';
+        stream_wrapper_unregister("php");
+        stream_wrapper_register("php", "MockPhpStream");
 
-    //gateway_bitpay($separator, $sessionid);
+        file_put_contents('php://input', '[' .
+            '{"code":1000,' .
+            '"amount":0.0195,' .
+            '"timestamp":"2013­12­02T16:16:29.612Z",' .
+            '"description":"2",' .
+            '"txType":"sale",' .
+            '"exRates":{"USD":1025},' .
+            '"buyerFields":{' .
+            '  "buyerName":"BitPay Customer",' .
+            '  "buyerAddress1":"3423 Piedmont Rd NE",' .
+            '  "buyerAddress2":"Suite 516",' .
+            '  "buyerCity":"Atlanta",' .
+            '  "buyerState":"GA",' .
+            '  "buyerZip":"30305",' .
+            '  "buyerEmail":"customer@bitpay.com",' .
+            '  "buyerPhone":"1­855­4­BITPAY"},' .
+            'invoiceId":"C8a5bQeRTPDineVSDCw6KJ",' .
+            '"sourceType":"invoice",' .
+            '"orderId":"2"},' .
+            '{"code":1001,' .
+            '"amount":-0.0002,' .
+            '"timestamp":"2013­12­02T16:16:29.612Z",' .
+            '"txType":"fee",' .
+            '"exRates":{"USD":1025},' .
+            '"buyerFields":{' .
+            '  "buyerName":"BitPay Customer",' .
+            '  "buyerAddress1":"3423 Piedmont Rd NE",' .
+            '  "buyerAddress2":"Suite 516","buyerCity":"Atlanta",' .
+            '  "buyerState":"GA",' .
+            '  "buyerZip":"30305",' .
+            '  "buyerEmail":"customer@bitpay.com",' .
+            '  "buyerPhone":"1­855­4­BITPAY"},' .
+            '"invoiceId":"C8a5bQeRTPDineVSDCw6KJ",' .
+            '"sourceType":"invoice",' .
+            '"orderId":"2"},' .
+            '{"code":1000,' .
+            '"amount":0.1093,' .
+            '"timestamp":"2014­01­06T19:01:09.522Z",' .
+            '"description":"Bill 1",' .
+            '"txType":"sale",' .
+            '"exRates":{"USD":915.0704604254529},' .
+            '"buyerFields":{},' .
+            '"invoiceId":"JHfkEPc212HThzB25ngz31",' .
+            '"sourceType":"invoice",' .
+            '"orderId":""},' .
+            '{"code":1001,' .
+            '"amount":-0.0011,' .
+            '"timestamp":"2014­01­06T19:01:09.522Z",' .
+            '"txType":"fee",' .
+            '"exRates":{"USD":915.0704604254529},' .
+            '"buyerFields":{},' .
+            '"invoiceId":"JHfkEPc212HThzB25ngz31",' .
+            '"sourceType":"invoice"}' .
+            ']'
+        );
 
-  }
+        $data = file_get_contents('php://input');
 
-  function testbitpay_callback() {
+        bitpay_callback();
 
-    require_once "phpmockstream.php";
-    $_GET['bitpay_callback'] = true;
-    stream_wrapper_unregister("php");
-    stream_wrapper_register("php", "MockPhpStream");
-    file_put_contents('php://input', '[
-    {"code":1000,
-    "amount":0.0195,
-    "timestamp":"2013­12­02T16:16:29.612Z",
-    "description":"2",
-    "txType":"sale",
-    "exRates":{"USD":1025},
-    "buyerFields":{
-      "buyerName":"BitPay Customer",
-      "buyerAddress1":"3423 Piedmont Rd NE",
-      "buyerAddress2":"Suite 516",
-      "buyerCity":"Atlanta",
-      "buyerState":"GA",
-      "buyerZip":"30305",
-      "buyerEmail":"customer@bitpay.com",
-      "buyerPhone":"1­855­4­BITPAY"},
-    "invoiceId":"C8a5bQeRTPDineVSDCw6KJ",
-    "sourceType":"invoice",
-    "orderId":"2"},
-    {"code":1001,
-    "amount":-0.0002,
-    "timestamp":"2013­12­02T16:16:29.612Z",
-    "txType":"fee",
-    "exRates":{"USD":1025},
-    "buyerFields":{
-      "buyerName":"BitPay Customer",
-      "buyerAddress1":"3423 Piedmont Rd NE",
-      "buyerAddress2":"Suite 516","buyerCity":"Atlanta",
-      "buyerState":"GA",
-      "buyerZip":"30305",
-      "buyerEmail":"customer@bitpay.com",
-      "buyerPhone":"1­855­4­BITPAY"},
-    "invoiceId":"C8a5bQeRTPDineVSDCw6KJ",
-    "sourceType":"invoice",
-    "orderId":"2"},
-    {"code":1000,
-    "amount":0.1093,
-    "timestamp":"2014­01­06T19:01:09.522Z",
-    "description":"Bill 1",
-    "txType":"sale",
-    "exRates":{"USD":915.0704604254529},
-    "buyerFields":{},
-    "invoiceId":"JHfkEPc212HThzB25ngz31",
-    "sourceType":"invoice",
-    "orderId":""},
-    {"code":1001,
-    "amount":-0.0011,
-    "timestamp":"2014­01­06T19:01:09.522Z",
-    "txType":"fee",
-    "exRates":{"USD":915.0704604254529},
-    "buyerFields":{},
-    "invoiceId":"JHfkEPc212HThzB25ngz31",
-    "sourceType":"invoice"}
-    ]');
-    $data = file_get_contents('php://input');
-    bitpay_callback();
-    stream_wrapper_restore("php");
-
-  }
+        stream_wrapper_restore("php");
+    }
 
 }
-
